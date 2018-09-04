f440cd032a16ef77fe71ce298739f4cf6189abd8
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-04 00:33:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,20 @@
 class Application
- 
+
   @@items = [Item.new("Sorry", "Justin Bieber"),
             Item.new("Hello","Adele")]
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
- 
-      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
- 
-      resp.write song.artist
+
+      item_title = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = @@items.find{|s| s.title == item_title}
+
+      resp.write item.price
     end
- 
+
     resp.finish
   end
 end
